ea8f062aa01f89f340617feb8776e76040bc0fca
Sofia <lsofmascastanio@gmail.com>
2022-11-17 18:04:21 +0100

    first commit

---
+++ src/main/java/com/sofia/dataloader/service/FestService.java
@@ -0,0 +1,16 @@
+package com.sofia.dataloader.service;
+
+import com.sofia.dataloader.domain.Fest;
+import com.sofia.dataloader.repository.FestRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FestService {
+
+    private FestRepository festRepository;
+
+    public Iterable<Fest> list() {
+        
+
+    }
+}
